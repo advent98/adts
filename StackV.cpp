eced6c69a189466b12276c74c30f7c1c4f0749d0
advent98 <advent98@users.noreply.github.com>
2018-03-28 11:33:35 -0400

    updates

--- StackV.cpp
+++ StackV.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
 #include "StackV.h"
+
+using namepsacestd;
+int stack::size()
+{
+	return data.size();
+}
+
+void stack::push(int val)
+{
+	data.push_back(val);
+}
+
+void stack::pop_back()
+{
+	data.pop_back();
+}
+
+int stack::top()
+{
+	int top = size()-1;
+	int topval = data[top];
+	return topval;
+}
+
+void stack::clear()
+{
+	  if(size()==0)
+	{
+		cout<<"The stack is empty";
+	}
+	  else
+	{
+		for(int i=0; i<top(); i++)
+	     {
+		data.pop_back();
+	     }
+	}
+}
+     
